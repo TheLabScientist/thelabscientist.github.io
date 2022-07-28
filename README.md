af16e7fa556cc1dfdfb09cebd57f1195ea1a2913
TheLabScientist <110190470+TheLabScientist@users.noreply.github.com>
2022-07-28 13:02:47 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# TheLabScientist.github.io
---
+++ main.js
@@ -0,0 +1,6 @@
+console.log('Started...');
+
+//Copyright Card Handle Click Event. Whe The Handle Is Clicked, The Whole Card Should Move Down
+function shiftCopyrightCardDown() {
+    console.log('Copyright Card Handle Clicked...');
+}
